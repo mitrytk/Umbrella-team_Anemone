9cf556be89cd932e2d37fc5b6d40d681bd635db4
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2022-12-19 15:24:45 +0100

    исправляет backdrop

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,6 +3,7 @@
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
+
       openModalThanksBtn: document.querySelector("[data-modal-thanks-open]"),
       closeModalThanksBtn: document.querySelector("[data-modal-thanks-close]"),
       modalThanks: document.querySelector("[data-modal-thanks]"),
@@ -12,8 +13,10 @@
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
     refs.closeModalThanksBtn.addEventListener("click", toggleModal);
+    
     refs.openModalThanksBtn.addEventListener("click", toggleModalThanks);
     refs.closeModalThanksBtn.addEventListener("click", toggleModalThanks);
+
     refs.openModalBtn.addEventListener("click", toggleModalFixed);
     refs.closeModalBtn.addEventListener("click", toggleModalFixed);
     refs.closeModalThanksBtn.addEventListener("click", toggleModalFixed);
