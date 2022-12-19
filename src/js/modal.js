da104c0e745f6c021f7cd81db1c835116427cad3
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2022-12-19 15:54:57 +0100

    замена write

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,7 +3,7 @@
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
-
+      
       openModalThanksBtn: document.querySelector("[data-modal-thanks-open]"),
       closeModalThanksBtn: document.querySelector("[data-modal-thanks-close]"),
       modalThanks: document.querySelector("[data-modal-thanks]"),
@@ -13,7 +13,7 @@
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
     refs.closeModalThanksBtn.addEventListener("click", toggleModal);
-    
+
     refs.openModalThanksBtn.addEventListener("click", toggleModalThanks);
     refs.closeModalThanksBtn.addEventListener("click", toggleModalThanks);
 
@@ -24,9 +24,14 @@
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
     }
+    // setTimeout(function toggleModalThanks(){ 
+    //   refs.modalThanks.classList.toggle("is-hidden");
+    //  }, 10000);
+
     function toggleModalThanks() {
-      refs.modalThanks.classList.toggle("is-hidden");
+      setTimeout(refs.modalThanks.classList.toggle("is-hidden"), 10000);
     }
+
     function toggleModalFixed() {
       refs.bodyFixed.classList.toggle("is-fixed");
     }
