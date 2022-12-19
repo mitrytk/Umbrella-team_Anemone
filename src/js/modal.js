f7445678509e8090a98246195a8b4bf704534c27
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2022-12-19 14:58:44 +0100

    Добавляет работу modal Thanks

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,13 +3,22 @@
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
+      openModalThanksBtn: document.querySelector("[data-modal-thanks-open]"),
+      closeModalThanksBtn: document.querySelector("[data-modal-thanks-close]"),
+      modalThanks: document.querySelector("[data-modal-thanks]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalThanksBtn.addEventListener("click", toggleModal);
+    refs.openModalThanksBtn.addEventListener("click", toggleModalThanks);
+    refs.closeModalThanksBtn.addEventListener("click", toggleModalThanks);
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
+    }
+    function toggleModalThanks() {
+      refs.modalThanks.classList.toggle("is-hidden");
     }
   })();
   (() => {
