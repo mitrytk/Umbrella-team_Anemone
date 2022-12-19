82a02933d56ca5b85fef5ee63cece421f9fe162b
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2022-12-19 17:02:13 +0100

    Добавляет modal follow

--- src/js/modal.js
+++ src/js/modal.js
@@ -36,3 +36,33 @@
       refs.bodyFixed.classList.toggle("is-fixed");
     }
   })();
+
+  // follow
+  (() => {
+    const refs = {
+      openModalFollowBtn: document.querySelector("[data-modal-follow-open]"),
+      closeModalFollowBtn: document.querySelector("[data-modal-follow-close]"),
+      modalFollow: document.querySelector("[data-modal-follow]"),
+    };
+  
+    refs.openModalFollowBtn.addEventListener("click", toggleModal);
+    refs.closeModalFollowBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modalFollow.classList.toggle("is-hidden");
+    }
+  })();
+  (() => {
+    const refs = {
+      openModalFollowBtn: document.querySelector("[data-modal-follow-open]"),
+      closeModalFollowBtn: document.querySelector("[data-modal-follow-close]"),
+      bodyFixed: document.querySelector("[data-body-fixed]"),
+    };
+  
+    refs.openModalFollowBtn.addEventListener("click", toggleModal);
+    refs.closeModalFollowBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.bodyFixed.classList.toggle("is-fixed");
+    }
+  })();
