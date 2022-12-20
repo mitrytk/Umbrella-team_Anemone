f9330fe934fdde74edd22d0c5f2422ae455ce9a5
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2022-12-20 10:46:03 +0100

    Добавляет якорные ссылки переход

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,18 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const menuBtnLinkRef = document.querySelector('.mobile__list');
 
   menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+  menuBtnLinkRef.addEventListener('click', () => {
     const expanded =
       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
