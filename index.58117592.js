37d66d9669cfbb54ae1861c3e8b8484c7afc8f83
mitrytk <90568941+mitrytk@users.noreply.github.com>
2022-12-21 13:11:28 +0000

    Deploying to gh-pages from @ mitrytk/Umbrella-team_Anemone@1c433e597085c7366b3d866c73fce7ba54893454 🚀

---
+++ index.58117592.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".blog__slider").slick({arrows:!0,dots:!1,adaptiveHeight:!1,slidesToShow:3,speed:300,easing:"linear",autoplay:!0,autoplaySpeed:1500,waitForAnimate:!1,centerMode:!0,variableWidth:!0})}));
+//# sourceMappingURL=index.58117592.js.map
--- index.c14ae5f3.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".blog__slider").slick({arrows:!0,dots:!1,adaptiveHeight:!1,slidesToShow:3,speed:300,easing:"linear",centerMode:!0,variableWidth:!0})}));
-//# sourceMappingURL=index.c14ae5f3.js.map
